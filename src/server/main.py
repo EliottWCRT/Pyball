f0c759bdd43db4bdb18b222d94d7303cb3a9d8e2
EliottWCRT <eliott.creative@gmail.com>
2023-11-08 15:19:31 +0100

    regles du jeu fini

--- src/server/main.py
+++ src/server/main.py
@@ -19,59 +19,94 @@ referee.moveTowards(18, 0)
 
 
 def resetStadium():
+    """1. Initialiser les règles du jeu"""
     agents = {
-    "DefenseurB": {
-        "team":0,
-        "x": 7,
-        "y": 7,
-        "color": (0,0,255)
+        "DefenseurB": {
+            "team":0,
+            "x": 7,
+            "y": 7,
+            "color": (0,0,255)
+            },
+        "AttaquantB1": {
+            "team":0,
+            "x": 15,
+            "y": 4,
+            "color": (0,0,255)
+            },
+        "AttaquantB2": {
+            "team":0,
+            "x": 15,
+            "y": 10,
+            "color": (0,0,255)
+            },
+        "DefenseurR": {
+            "team": 1,
+            "x": 28, 
+            "y": 7,
+            "color": (255,0,0)
         },
-    "AttaquantB1": {
-        "team":0,
-        "x": 15,
-        "y": 4,
-        "color": (0,0,255)
+        "AttaquantR1": {
+            "team": 1,
+            "x": 20, 
+            "y": 10,
+            "color": (255,0,0)
         },
-    "AttaquantB2": {
-        "team":0,
-        "x": 15,
-        "y": 10,
-        "color": (0,0,255)
-        },
-    "DefenseurR": {
-        "team": 1,
-        "x": 28, 
-        "y": 7,
-        "color": (255,0,0)
-    },
-    "AttaquantR1": {
-        "team": 1,
-        "x": 20, 
-        "y": 10,
-        "color": (255,0,0)
-    },
-    "AttaquantR2": {
-        "team": 1,
-        "x": 20, 
-        "y": 4,
-        "color": (255,0,0)
+        "AttaquantR2": {
+            "team": 1,
+            "x": 20, 
+            "y": 4,
+            "color": (255,0,0)
+
+        }
     }
-}
+
+
+    
+    # Regle pour gérer la balle profile indice 4 : collision =False (Pouvoir marcher dessus), invincible =True, hitCollision =0dmg, ammoIni =0, spawnArea(balle au milieu equipe 2) 
+    colisionBallRule = referee.game["collision"]
+    colisionBallRule[4] = False
+    referee.ruleArena("collision", colisionBallRule)
+    
+
+    invicibleRule = referee.game["invincible"]
+    invicibleRule[4] = True
+    invicibleRule[0] = True
+    referee.ruleArena("invincible", invicibleRule)
+    
+    hitCollision = referee.game["hitCollision"]
+    hitCollision[4] = 0
+    hitCollision[0] = 0
+    referee.ruleArena("hitCollision", hitCollision)
+
+
+    ammoIni = referee.game["ammoIni"]
+    ammoIni[4] = 100
+    referee.ruleArena("ammoIni", ammoIni)
+
+    # Regle pour gérer les joueurs profile indice 0 : invincible =True, hitCollision =0dmg, infiniteAmmo, spawnArea(sur toute la carte), range max 
+    
+
+    infiniteAmmo = referee.game["infiniteAmmo"]
+    infiniteAmmo[0] = True
+    referee.ruleArena("infiniteAmmo", infiniteAmmo)
+
+    rangePlayers = referee.game["range"]
+    rangePlayers [0] = 0
+    referee.ruleArena("range", rangePlayers)
+
+
+    # Regle pour gérer l'affichage de l'arène : gridsColumns, grids Rows
+    #2. Initialisition de l'arbitre, des teams et des agents : 
 
     for agentId, attributes in agents.items():
         for attributeKey, attributeValue, in attributes.items():
             referee.rulePlayer(agentId, attributeKey, attributeValue)
     referee.update()
+    #3 Fermer les portes de l'arène 
+
+resetStadium()
 
 
-#1. Initialiser les règles du jeu : Tir 
-# Regle pour gérer la balle profile indice 4 : collision =False (Pouvoir marcher dessus), invincible =True, hitCollision =0dmg, ammoIni =0, spawnArea(balle au milieu equipe 2), 
-# Regle pour gérer les joueurs profile indice 0 : invincible =True, hitCollision =0dmg, infiniteAmmo, spawnArea(sur toute la carte), range max 
-# Regle pour gérer l'affichage de l'arène : gridsColumns, grids Rows
-
-#2. Initialisition de l'arbitre, des teams et des agents : 
-
-#3 Fermer les portes de l'arène 
 
 #4 Boucle principale du jeu
 #4.2 : Joueur a la balle il peut tirer ou bien se deplacer
