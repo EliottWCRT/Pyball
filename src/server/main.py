f6084df458466e5260d4bafb55e84dab965bd535
EliottWCRT <eliott.creative@gmail.com>
2023-11-10 10:55:12 +0100

    scoring minuteur fini

--- src/server/main.py
+++ src/server/main.py
@@ -1,4 +1,5 @@
 import copy
+import time
 import j2l.pytactx.agent as pytactx
 import os
 from dotenv import load_dotenv
@@ -19,6 +20,14 @@ referee.moveTowards(18, 0)
 
 stadiumWidth= 37
 stadiumHeight= 17
+
+def timestampToStr(sec):
+   sec = sec % (24 * 3600)
+   hour = sec // 3600
+   sec %= 3600
+   min = sec // 60
+   sec %= 60
+   return "%02d:%02d:%02d" % (hour, min, sec) 
 
 def resetStadium():
     """1. Initialiser les règles du jeu"""
@@ -123,42 +132,65 @@ players= {}
 oldPlayers= {}
 scoreEquipeB=0
 scoreEquipeR=0
-
+gameOver=False
+timeLimit=1*60
+gameStartTime=referee.game["t"]
 #4 Boucle principale du jeu
-while True:
+
+while gameOver==False:
+    referee.update()
+    while "ball" not in referee.range:
+        referee.update()
+        referee.ruleArena("info", "Initilisation de la balle ⚽️")
+
     players=copy.deepcopy(referee.range)
     ball=players["ball"] 
     xBall, yBall = ball["x"],ball["y"]
-    referee.update()
-    
+    # 4.2 : Si le temps du jeu est écoulé 
+    elapsedTime=(referee.game["t"]-gameStartTime)/1000
+    gameOver=elapsedTime>=timeLimit
+    referee.ruleArena("info", f" Equipe bleu🔵 {scoreEquipeB} - {scoreEquipeR} 🔴Equipe Rouge  ⏳{timestampToStr(elapsedTime)}")
 
     
     # 4.2 : Si la balle arrive dans un but d'une équipe
     if 0<=xBall<1 and -1<=yBall-stadiumHeight//2<=+1:
         ...
-        print("équipe bleu a marqué un but")
+    # Rajoute +1 au score de l'équipe opposée
+        print("équipe rouge a marqué un but")
         scoreEquipeB+=1
-        referee.ruleArena("info", f"Equipe bleu {scoreEquipeB} - {scoreEquipeR}")
+    # Replacer les joueurs et la balle a leurs états initiaux en prennant un certain temps
         resetStadium()
 
     if stadiumWidth-1<=xBall<stadiumWidth and -1<=yBall-stadiumHeight//2<=+1:
         ...
-        print("équipe rouge a marqué un but")
+        print("équipe bleu a marqué un but")
+        scoreEquipeR+=1
         resetStadium()
-    # Rajoute +1 au score de l'équipe opposée
-    # Replacer les joueurs et la balle a leurs états initiaux en prennant un certain temps
+
+# Afficher le commentaire selon le score quand le jeu est terminé
+if scoreEquipeB>scoreEquipeR:
+    referee.ruleArena("info", "🏆 l'équipe Bleu a gagné")
+elif scoreEquipeR>scoreEquipeB:
+    referee.ruleArena("info", "🏆 l'équipe Rouge a gagné")
+else:
+    referee.ruleArena("info", "🏆 égalité")
+referee.ruleArena("pause", True)
+time.sleep(1)
+referee.update()
+
+
 
     
+        
+
 
 
 #4.2 : Joueur a la balle il peut tirer ou bien se deplacer
-# Tirer:Appliquer une force d'acceleration sur l'agent ball et mettre a false le Ownership de la ball du joueur 
+# Tirer:Appliquer une force d'acceleration sur l'agent ball et mettre a false le sur Ownership de la ball du joueur 
 # Se deplacer avec la balle: Mettre a jour la position de l'agent Ball et mettre la position du Owner
 #4.2 : Le joueur n'a pas la balle: se deplacer indépandament de la balle, ou bien il peut intercepter la balle
 # intercepter la balle: a une distance de une case de la balle et si une condition random est vrai 1/5, transferer le Ownership de la balle au joueur intercepteur
 
 
-# 4.2 : Si le temps du jeu est écoulé 
-r
 # Le jeu est mis en pause pendant 30 secondes 
 
